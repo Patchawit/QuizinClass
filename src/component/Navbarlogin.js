c39835e7826d40cd6ed45995ac0ffe7b60558d39
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-01-22 12:26:19 +0700

    login

--- src/component/Navbarlogin.js
+++ src/component/Navbarlogin.js
@@ -9,10 +9,7 @@ export default function Navbarlogin() {
                 <Navbar bg="light" className='bar'> 
                         <Navbar.Brand className='bar'>Quiz in Class</Navbar.Brand>
                         <Navbar.Brand className='bar2'>เข้าสู่ระบบ</Navbar.Brand>
-                </Navbar>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                </Navbar>               
             </div>
     )
 }
