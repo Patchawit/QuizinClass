2ddd4883052ac9533b7e906f52f86ed4f2246218
Narawich2013 <narawich2013@gmail.com>
2022-01-23 18:39:40 +0700

    navbar edit

--- src/component/Navbarlogin.js
+++ src/component/Navbarlogin.js
@@ -7,7 +7,7 @@ export default function Navbarlogin() {
     return (
             <div>
                 <Navbar bg="light" className='bar'> 
-                        <Navbar.Brand className='bar'>Quiz in Class</Navbar.Brand>
+                        <Navbar.Brand className='bar'><h5>Quiz  <font color="#FFAD32"> in </font> Class</h5></Navbar.Brand>
                         <Navbar.Brand className='bar2 text-warning '><h2><strong>เข้าสู่ระบบ</strong></h2></Navbar.Brand>
                 </Navbar>               
             </div>
