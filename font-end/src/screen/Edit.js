11b3d2e5190de91154d1dca1e4c86e37885cb135
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-01-27 00:22:59 +0700

    center

--- font-end/src/screen/Edit.js
+++ font-end/src/screen/Edit.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Edit() {
     return (
-        <div>
+        <div width="100%" height="100%">
             <h1>แก้ไขชุดคำถาม</h1>
+            <div className='center'>
+            </div>
         </div>
+        
     )
 }
