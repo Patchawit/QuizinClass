b7d73f23c03c9a4b987da6167075abd1de8dc5fc
skaferun9 <43022322+skaferun9@users.noreply.github.com>
2022-01-26 23:44:31 +0700

    update

--- font-end/src/screen/Create.js
+++ font-end/src/screen/Create.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 export default function Create() {
     const [subjectTitle, setSubjectTitle] = useState('')
-    const [allSubject, setAllSubject] = useState({})
+    const [isLoading, setLoadind] = useState(false)
+    const [allSubject, setAllSubject] = useState([])
     const onSubjectChange = (event) => {
         setSubjectTitle(event.target.value)
     }
@@ -17,28 +18,38 @@ export default function Create() {
 
     }
     useEffect(async () => {
+        setLoadind(true)
         await fetch("http://localhost:5000/admin/category", {
             headers: {
                 'Content-Type': 'application/json',
             },
             method: "GET",
         })
-            .then(res => { return res.json() }).then(result => {
+            .then(res => {
+                return res.json()
+            }).then(result => {
                 console.log(result)
-                const subjects = result
-                setAllSubject(subjects)
-                console.log(allSubject)
+                return setAllSubject(result.allSubject)
             })
+        setLoadind(false)
     }, [])
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div>
             <h1>สร้างวิชา</h1>
             <input value={subjectTitle} onChange={onSubjectChange} className="form-control form-control-lg" type="text" placeholder="กรอกชื่อวิชา"></input>
             <button type="button" onClick={onSubmit} className="btn btn-warning btn-lg text-dark mt-5">บันทึก</button>
-
+            <ul>
+                {allSubject?.map((subject, key) => {
+                    console.log(subject)
+                    return <li key={key}>{subject.subjecttitle}</li>
+                })}
+            </ul>
         </div>
-
     )
 }
 
