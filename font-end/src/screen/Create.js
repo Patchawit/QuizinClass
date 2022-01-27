dbfd12b41976f48bc519242ce4578febd7271bfe
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-01-27 13:00:10 +0700

    ui

--- font-end/src/screen/Create.js
+++ font-end/src/screen/Create.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import CloseButton from 'react-bootstrap/CloseButton'
 export default function Create() {
     const [subjectTitle, setSubjectTitle] = useState('')
     const [isLoading, setLoadind] = useState(false)
@@ -40,15 +41,22 @@ export default function Create() {
 
     return (
         <div>
-            <h1>ชื่อหมวดหมู่วิชา</h1>
-            <input value={subjectTitle} onChange={onSubjectChange} className="form-control form-control-lg namesub" type="text" placeholder="กรอกชื่อวิชา"></input>
-            <button type="button" onClick={onSubmit} className="btn btn-warning btn-lg text-dark btnsave">บันทึก</button>
-            <ul>
-                {allSubject?.map((subject, key) => {
-                    console.log(subject)
-                    return <li key={key}>{subject.subjecttitle}</li>
-                })}
-            </ul>
+            <div className="center">
+                <div className="namesubject">
+                    <h3>ชื่อหมวดหมู่วิชา</h3>
+                    <input value={subjectTitle} onChange={onSubjectChange} className="form-control form-control-lg namesub" type="text" placeholder="กรอกชื่อวิชา"></input>
+                </div>
+                <button type="button" onClick={onSubmit} className="btn btn-warning btn-lg text-dark btnsave">บันทึก</button>
+                <ul className="name">
+                    <h3>ชื่อหมวดหมู่วิชา</h3>
+                    {allSubject?.map((subject, key) => {
+                        console.log(subject)
+                        return <li key={key}>{subject.subjecttitle}
+                                    <CloseButton aria-label="Hide" className="btnclose"/>
+                                </li>
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
--- font-end/src/screen/Profile.js
+++ font-end/src/screen/Profile.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div className="flex flex-col w-full justify-center text-center lg:text-left lg:w-2/3">
-      <h1 className="mb-6 text-5xl font-bold leading-tight uppercase">
+    <div>
+      <h1 className="mb-6 text-5xl font-bold leading-tight uppercase text-center">
         ข้อมูลส่วนตัว
       </h1>
       <div className='center'>
         <div className='profile'>
-          <div>
-            <img src="https://media.discordapp.net/attachments/695119707010891796/935944609610219540/iconfinder-send-4341325_120524_1.png" alt="" width="10%" height="10%" />
+          <img src="https://media.discordapp.net/attachments/695119707010891796/935944609610219540/iconfinder-send-4341325_120524_1.png" alt="" width="30%" height="70%" />
+          <div className="info">
+            <p>
+              รหัสนักศึกษา :
+            </p>
+            <p>
+              ชื่อ - นามสกุล :
+            </p>
           </div>
-          <p className="leading-normal text-xl mb-8">
-            รหัสนักศึกษา :
-          </p>
-          <p className="leading-normal text-xl mb-8">
-            ชื่อ - นามสกุล :
-          </p>
         </div>
-        <button type="button" class="btn btn-warning btn-lg text-white mt-5">ตรวจสอบคะแนน</button>
+        <button type="button" className="btn btn-warning btn-lg text-white mt-5 btnmyscore">ตรวจสอบคะแนน</button>
       </div>
     </div>
   )
