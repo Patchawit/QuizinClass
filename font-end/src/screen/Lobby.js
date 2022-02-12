3e157876b124367ad41ad24575c26c4f200ad245
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-02-12 10:47:27 +0700

    ui

--- font-end/src/screen/Lobby.js
+++ font-end/src/screen/Lobby.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function Lobby() {
   return (
-    <div>
-      <h1 className="mb-6 text-5xl font-bold leading-tight uppercase text-center">
-        Lobby Room
+    <div className='lobby'>
+      <h1 className="center">
+        ชื่อชุดคำถาม
       </h1>
+      <div className='center'>
+        <div className='ready'>
+          <img src="https://media.discordapp.net/attachments/867880540602368052/936140224852021258/itlogo.png" alt="" width="35%" height="70%" />
+          <h1>เตรียมตัวทำแบบทดสอบ</h1>
+        </div>
+      </div>
     </div>
   )
 }
