79d7be381d152bd7ddda150956fc088d8da35313
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-02-16 13:45:06 +0700

    ui

--- font-end/src/screen/Breakoutsession.js
+++ font-end/src/screen/Breakoutsession.js
@@ -2,15 +2,39 @@ import React from 'react'
 
 export default function Breakoutsession() {
   return (
-    <div className='lobby'>
-      <h1 className="center">
-        ชื่อชุดคำถาม
-      </h1>
-      <div className='center'>
-        <div className='ready'>
-          <img src="https://media.discordapp.net/attachments/867880540602368052/936140224852021258/itlogo.png" alt="" width="35%" height="70%" />
-          <h1>Breakoutsession</h1>
+    <div className='breakout'>
+      <div className='left'>
+        <div className='headleft'>
+          <h3>ชื่อชุดคำถาม</h3>
         </div>
+        <div className='questleft'>
+          <div className="namequestion">
+                <h3>ข้อที่ 1</h3>
+                <input className="form-control form-control-lg nameque3" type="text" placeholder=""></input>
+          </div>
+          <div className="namequestion">
+                <h3>1</h3>
+                <input className="form-control form-control-lg nameque2" type="text" placeholder=""></input>
+          </div>
+          <div className="namequestion">
+                <h3>2</h3>
+                <input className="form-control form-control-lg nameque2" type="text" placeholder=""></input>
+          </div>
+          <div className="namequestion">
+                <h3>3</h3>
+                <input className="form-control form-control-lg nameque2" type="text" placeholder=""></input>
+          </div>
+          <div className="namequestion">
+                <h3>4</h3>
+                <input className="form-control form-control-lg nameque2" type="text" placeholder=""></input>
+          </div>
+          <button type="button" className="btn btn-warning btn-lg text-dark btnnext">ต่อไป</button>
+        </div>
+      </div>
+      <div className='right'>
+        <h3>61000000</h3>
+        <h3>61000000</h3>
+        <input className="form-control form-control-lg chat" type="text" placeholder=""></input>
       </div>
     </div>
   )
--- font-end/src/screen/Lobby.js
+++ font-end/src/screen/Lobby.js
@@ -8,9 +8,112 @@ export default function Lobby() {
       </h1>
       <div className='center'>
         <div className='ready'>
-          <img src="https://media.discordapp.net/attachments/867880540602368052/936140224852021258/itlogo.png" alt="" width="35%" height="70%" />
-          <h1>เตรียมตัวทำแบบทดสอบ</h1>
+          <div className='room'>
+            <a>Room1</a>
+            <p>61000000</p>
+            <p>61000000</p>
+          </div>
+          <div className='room'>
+            <a>Room2</a>
+          </div>
+          <div className='room'>
+            <a>Room3</a>
+          </div>
+          <div className='room'>
+            <a>Room4</a>
+          </div>
+          <div className='room'>
+            <a>Room5</a>
+          </div>
+          <div className='room'>
+            <a>Room6</a>
+          </div>
         </div>
+
+        <div className='ready'>
+          <div className='room'>
+            <a>Room7</a>
+          </div>
+          <div className='room'>
+            <a>Room8</a>
+          </div>
+          <div className='room'>
+            <a>Room9</a>
+          </div>
+          <div className='room'>
+            <a>Room10</a>
+          </div>
+          <div className='room'>
+            <a>Room11</a>
+          </div>
+          <div className='room'>
+            <a>Room12</a>
+          </div>
+        </div>
+
+        <div className='ready'>
+          <div className='room'>
+            <a>Room13</a>
+          </div>
+          <div className='room'>
+            <a>Room14</a>
+          </div>
+          <div className='room'>
+            <a>Room15</a>
+          </div>
+          <div className='room'>
+            <a>Room16</a>
+          </div>
+          <div className='room'>
+            <a>Room17</a>
+          </div>
+          <div className='room'>
+            <a>Room18</a>
+          </div>
+        </div>
+
+        <div className='ready'>
+          <div className='room'>
+            <a>Room19</a>
+          </div>
+          <div className='room'>
+            <a>Room20</a>
+          </div>
+          <div className='room'>
+            <a>Room21</a>
+          </div>
+          <div className='room'>
+            <a>Room22</a>
+          </div>
+          <div className='room'>
+            <a>Room23</a>
+          </div>
+          <div className='room'>
+            <a>Room24</a>
+          </div>
+        </div>
+
+        <div className='ready'>
+          <div className='room'>
+            <a>Room25</a>
+          </div>
+          <div className='room'>
+            <a>Room26</a>
+          </div>
+          <div className='room'>
+            <a>Room27</a>
+          </div>
+          <div className='room'>
+            <a>Room28</a>
+          </div>
+          <div className='room'>
+            <a>Room29</a>
+          </div>
+          <div className='room'>
+            <a>Room30</a>
+          </div>
+        </div>
+      
       </div>
     </div>
   )
