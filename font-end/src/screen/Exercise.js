618702f9cffd44688307b5e0381c85607a4d016a
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-02-24 23:55:49 +0700

    ui

--- font-end/src/screen/Exercise.js
+++ font-end/src/screen/Exercise.js
@@ -1,27 +1,27 @@
 import { Form } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react'
 import {
-    useParams
+        useParams
 } from "react-router-dom";
 
 export default function Exercise() {
-    const [EditQuestion, setEditQuestion] = useState()
-    let { soqId } = useParams();
-    useEffect(async () => {
+        const [EditQuestion, setEditQuestion] = useState()
+        let { soqId } = useParams();
+        useEffect(async () => {
 
-        console.log(soqId)
-        await fetch(`http://localhost:5000/admin/Editque/${soqId}`)
-                .then(res => {
-                        return res.json()
-                })
-                .then(result => {
-                        setEditQuestion(result.setOfQuestion)
-                        console.log(result)
-                })
-    }, [])
-  return (
-    <div>
-          <div className='center qtitle'>
+                console.log(soqId)
+                await fetch(`http://localhost:5000/admin/Editque/${soqId}`)
+                        .then(res => {
+                                return res.json()
+                        })
+                        .then(result => {
+                                setEditQuestion(result.setOfQuestion)
+                                console.log(result)
+                        })
+        }, [])
+        return (
+                <div>
+                        <div className='center qtitle'>
                                 {
                                         EditQuestion?.soqtitle
                                         // EditQuestion && EditQuestion?.map((setofQuestion) => {
@@ -29,29 +29,37 @@ export default function Exercise() {
                                         // })
                                 }
                         </div>
-    {
-        EditQuestion?.questions?.map(question => {
-                return <div className='groupcreate'><div className='questsave'>
-                        <p>ข้อที่...
-                                <Form.Control id="disabledTextInput" className='inputquest' placeholder={question.questionstitle} disabled />
-                        </p>{
-                                question.choices.map(choice => {
-                                        return <div>
-                                                <p>ตัวเลือกที่...
-                                                        <Form.Control id="disabledTextInput" className='inputques' placeholder={choice.choiceTitle} disabled />
-                                                </p>
-                                        </div>
+                        {
+                                EditQuestion?.questions?.map(question => {
+                                        return <div className='groupcreate'><div className='questsave'>
+                                                <p>ข้อที่...
+                                                        {question.questionstitle}
+                                                </p>{
+                                                        question.choices.map(choice => {
+                                                                return <div>
+                                                                        <p>ตัวเลือกที่...
+                                                                                <Form>
+                                                                                        {['radio'].map((type) => (
+                                                                                                <div className="mb-3">
+                                                                                                        <Form.Check
+                                                                                                                label={choice.choiceTitle}
+                                                                                                                name="group1"
+                                                                                                                type={type}
+                                                                                                        />
+                                                                                                </div>
+                                                                                        ))}
+                                                                                </Form>
+                                                                        </p>
+                                                                </div>
+                                                        })
+
+                                                } <button type="button" className="btn btn-warning btn-lg text-dark">ต่อไป</button>
+
+                                        </div></div>
                                 })
+                        }
 
-                        } <p>คำตอบ
-                                <Form.Control id="disabledTextInput" className='inputquest' placeholder={question.ans} disabled />
-                        </p>
-                       
-                </div></div>
-        })
-}
+                </div>
 
-    </div>
-    
-  )
+        )
 }
