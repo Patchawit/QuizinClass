64b2564fcced282ea8e9ee891a95bcaf93596bf8
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-03-20 22:03:38 +0700

    back

--- back-end/App.js
+++ back-end/App.js
@@ -34,7 +34,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/auth", authRoutes);
-app.use(passportSetup);
+
 
 mongoose.connect(mongodburi)
   .then(result => {
