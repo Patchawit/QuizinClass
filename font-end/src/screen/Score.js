236c68cd69cdd5f8e00c83e25d976c7af0ab9a0b
Narawich2013 <narawich2013@gmail.com>
2022-04-12 18:07:31 +0700

    soqid

--- font-end/src/screen/Score.js
+++ font-end/src/screen/Score.js
@@ -93,67 +93,27 @@ export default function Score() {
                         console.log(score.soqid, score.score)
                         return <h1>{Content}</h1>
                     })
-                return <div><h1>{student.name}</h1>{studentscore}</div>}
+                return <div><h1>{student.email}</h1>{studentscore}</div>}
                 )}
             </div>
-            {/* <div className='center'>
+            <div className='center'>
                 <div className='table'>
                     <Table striped bordered>
                         <thead>
                             <tr>
                                 <th>รหัสนักศึกษา ชื่อ-สกุล</th>
                                 <th>ชุดที่ 1</th>
-                                <th>ชุดที่ 2</th>
-                                <th>ชุดที่ 3</th>
-                                <th>ชุดที่ 4</th>
-                                <th>ชุดที่ 5</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>61000001 กวิน บินได้</td>
                                 <td>20</td>
-                                <td>19</td>
-                                <td>18</td>
-                                <td>20</td>
-                                <td>20</td>
-                            </tr>
-                            <tr>
-                                <td>61000002 กวิน บินได้</td>
-                                <td>20</td>
-                                <td>19</td>
-                                <td>18</td>
-                                <td>19</td>
-                                <td>19</td>
-                            </tr>
-                            <tr>
-                                <td>61000003 กวิน บินได้</td>
-                                <td>20</td>
-                                <td>19</td>
-                                <td>18</td>
-                                <td>18</td>
-                                <td>18</td>
-                            </tr>
-                            <tr>
-                                <td>61000004 กวิน บินได้</td>
-                                <td>20</td>
-                                <td>19</td>
-                                <td>18</td>
-                                <td>17</td>
-                                <td>17</td>
-                            </tr>
-                            <tr>
-                                <td>61000003 กวิน บินได้</td>
-                                <td>20</td>
-                                <td>19</td>
-                                <td>18</td>
-                                <td>16</td>
-                                <td>16</td>
                             </tr>
                         </tbody>
                     </Table>
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
