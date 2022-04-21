6fb1d053dce1f004ddc9b9e36ae10253b9a7e2d8
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-04-21 10:54:08 +0700

    ui

--- font-end/src/screen/Ready.js
+++ font-end/src/screen/Ready.js
@@ -3,6 +3,24 @@ import React from 'react'
 
 export default function Ready() {
     return (
-    <div>AAAAAAAAA</div>
+    <div className='center'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-6 qrcodestyle'>
+            <div className='qrcode'>
+            </div>
+            <p>จำนวนคน : 0</p>
+          </div>
+          <div className='col-6 qrcodestart'>
+            <div className='starting'> 
+              <button type="button" className="btn btn-warning btn-lg text-white">เริ่มทำแบบทดสอบ</button>
+            </div>
+            <div className='namestart'>
+              <button type="button" className="btn btn-warning btn-lg text-white">รายชื่อผู้ทำแบบทดสอบ</button>    
+            </div>        
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
