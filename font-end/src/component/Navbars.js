e3ae6bccea20594d3b10c30961e28cf5cd67a1a3
Patchawit Muensoontorn <61070134@kmitl.ac.th>
2022-04-23 16:51:41 +0700

    Update Navbars.js

--- font-end/src/component/Navbars.js
+++ font-end/src/component/Navbars.js
@@ -21,60 +21,60 @@ export default function Navbars(props) {
 
     let content
     if (user.usertype === "Teacher") {
-        content = <div>
-            <Navbar bg="light" expand="lg" className='mainbar'>
-                <Navbar.Brand href="/" className='logo'><h3><strong>Quiz <font color="#FFAD32"> in </font> Class</strong></h3></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/" className='subbar2'>ชุดคำถาม</Nav.Link>
-                        <NavDropdown title="หมวดหมู่วิชา" id="basic-nav-dropdown" className='subbar2'>
-                            <NavDropdown.Item href="/createquiz">สร้างวิชา</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="/editquiz" className='subbar2'>แก้ไขคำถาม</Nav.Link>
-                        <Nav.Link href="/scorequiz" className='subbar2'>ผลคะแนน</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text className='subbar3'>
-                        เข้าสู่ระบบโดย : <a href="/profile"> <font color="#FFAD32">{props.loginBy}</font></a>
-                    </Navbar.Text>
-                    {/* <Button variant="dark">ออกจากระบบ</Button> */}
-                    <GoogleLogout
-                        clientId={clientId}
-                        buttonText="ออกจากระบบ"
-                        onLogoutSuccess={onLogoutSuccess}
-                    />
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
+
+        content = <Navbar bg="light" expand="lg" className='mainbar'>
+            <Navbar.Brand href="/" className='logo'><h3><strong>Quiz <font color="#FFAD32"> in </font> Class</strong></h3></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/" className='subbar2'>ชุดคำถาม</Nav.Link>
+                    <NavDropdown title="หมวดหมู่วิชา" id="basic-nav-dropdown" className='subbar2'>
+                        <NavDropdown.Item href="/createquiz">สร้างวิชา</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="/editquiz" className='subbar2'>แก้ไขคำถาม</Nav.Link>
+                    <Nav.Link href="/scorequiz" className='subbar2'>ผลคะแนน</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text className='subbar3'>
+                    เข้าสู่ระบบโดย : <a href="/profile"> <font color="#FFAD32">{props.loginBy}</font></a>
+                </Navbar.Text>
+                {/* <Button variant="dark">ออกจากระบบ</Button> */}
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="ออกจากระบบ"
+                    onLogoutSuccess={onLogoutSuccess}
+                />
+            </Navbar.Collapse>
+        </Navbar>
+
 
     }
     else {
-        content = <div>
-            <Navbar bg="light" expand="lg" className='mainbar'>
-                <Navbar.Brand href="/" className='logo'><h3><strong>Quiz <font color="#FFAD32"> in </font> Class</strong></h3></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        Student
-                    </Nav>
-                </Navbar.Collapse>
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text className='subbar3'>
-                        เข้าสู่ระบบโดย : <a href="/profile"> <font color="#FFAD32">{props.loginBy}</font></a>
-                    </Navbar.Text>
-                    {/* <Button variant="dark">ออกจากระบบ</Button> */}
-                    <GoogleLogout
-                        clientId={clientId}
-                        buttonText="ออกจากระบบ"
-                        onLogoutSuccess={onLogoutSuccess}
-                    />
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
+
+        content = <Navbar bg="light" expand="lg" className='mainbar'>
+            <Navbar.Brand href="/" className='logo'><h3><strong>Quiz <font color="#FFAD32"> in </font> Class</strong></h3></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                     Student 
+                </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text className='subbar3'>
+                    เข้าสู่ระบบโดย : <a href="/profile"> <font color="#FFAD32">{props.loginBy}</font></a>
+                </Navbar.Text>
+                {/* <Button variant="dark">ออกจากระบบ</Button> */}
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="ออกจากระบบ"
+                    onLogoutSuccess={onLogoutSuccess}
+                />
+            </Navbar.Collapse>
+        </Navbar>
+
 
     }
-    return { content }
+    return (<>{ content }</>)
 
 }
