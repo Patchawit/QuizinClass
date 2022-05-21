2b55d359fcb43d16bee3b4819af36c533990a388
skaferun9 <43022322+skaferun9@users.noreply.github.com>
2022-05-22 00:15:13 +0700

    update

--- font-end/src/screen/StudentScore.js
+++ font-end/src/screen/StudentScore.js
@@ -27,46 +27,29 @@ export default function StudentScore() {
   const [Studentlist, setStudentList] = useState()
   const [soqId, setSoqId] = useState()
   const [soq, setSoq] = useState()
+
   useEffect(async () => {
     console.log(user.email)
     await fetch(`http://localhost:7050/admin/GetEverDoSoq/${user.email}`, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: "GET",
+    })
+      .then(res => { return res.json() })
+      .then(result => {
+        console.log(result.listOfSoq)
+        // const filteredSoq = result.SetOfQuestion.filter(soq)
+        setListOfSoq(result.listOfSoq)
+        return console.log(result.listOfSoq)
       })
-        .then(res => { return res.json() })
-        .then(result => {
-          console.log(result.listOfSoq)
-          // const filteredSoq = result.SetOfQuestion.filter(soq)
-          setListOfSoq(result.listOfSoq)
-          return console.log(result.listOfSoq)
-        })
   }, [])
 
- 
-
-
-  // const onSubjectChangeHandler = async (e) => {
-  //   e.preventDefault();
-  //   if (e.target.value !== "default") {
-  //     await fetch(`http://localhost:7050/admin/SetOfQuestion/${user.email}/${e.target.value}`, {
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       method: "GET",
-  //     })
-  //       .then(res => { return res.json() })
-  //       .then(result => {
-  //         console.log(result.SetOfQuestion)
-  //         // const filteredSoq = result.SetOfQuestion.filter(soq)
-  //         setListOfSoq(result.SetOfQuestion)
-  //         return console.log(result)
-  //       })
-  //   }
-  // }
 
   const onSoqChangeHandler = async (e) => {
+    if (e.target.value === 'default') {
+      return;
+    }
     setSoqId(e.target.value)
     counter = -1
     await fetch(`http://localhost:7050/admin/Editque/${e.target.value}`, {
@@ -77,7 +60,6 @@ export default function StudentScore() {
     })
       .then(res => { return res.json() })
       .then(result => {
-        console.log(result.setOfQuestion)
         setSoq(result.setOfQuestion)
       })
 
@@ -89,7 +71,6 @@ export default function StudentScore() {
     })
       .then(res => { return res.json() })
       .then(result => {
-        console.log(result)
         setStudentList(result)
       })
   }
@@ -118,47 +99,48 @@ export default function StudentScore() {
       <div>
         {soq && soq.questions.map(question => {
           return <div className='answeruser groupcreate'>
-            <h3>คำถาม : {question.questionstitle}</h3>
-            {question.imgUrl === "images/1x1.png" ? <div></div> : <img src={`http://localhost:7050/` + question.imgUrl} />}
-            {question.choices.map(choice => {
+            <h3>คำถาม : {question?.questionstitle}</h3>
+            {question?.imgUrl === "images/1x1.png" ? <div></div> : <img src={`http://localhost:7050/` + question?.imgUrl} />}
+            {question?.choices.map(choice => {
               return <div className='container'><div className='row'><div className='col-1'><p>ตัวเลือก:</p></div>
-               <div className='col-2'>
-                {choice.isCorrect == true ?
-                  <h3 style={{ color: "green" }}>
-                    {choice.choiceTitle}
-                  </h3> :
-                  <h3 style={{ color: "red" }}>
-                    {choice.choiceTitle}
-                  </h3>
+                <div className='col-2'>
+                  {choice?.isCorrect == true ?
+                    <h3 style={{ color: "green" }}>
+                      {choice?.choiceTitle}
+                    </h3> :
+                    <h3 style={{ color: "red" }}>
+                      {choice?.choiceTitle}
+                    </h3>
 
-                }
+                  }
                 </div>
-                
+
               </div></div>
             })}
             <div>
               {Studentlist && Studentlist?.users?.map((student) => {
-                const studentAnswer = student?.history.filter(data => {
+                let content;
+                const studentAnswer = student?.history?.filter(data => {
                   return data.soqid === soqId;
                 })
-                let content = studentAnswer.map((ans, index) => {
+                content = studentAnswer?.map((ans, index) => {
                   return ans.studentAns
                 })
-                content = content[0].map(data => {
-                  return { Title: data.ans.choiceTitle, Correct: data.ans.isCorrect }
+
+                content = content[0]?.map(data => {
+                  return { Title: data?.ans?.choiceTitle, Correct: data?.ans?.isCorrect }
                 })
+
+
                 counter++
-                // return (content[counter])
-                console.log(studentAnswer)
-                if (content[counter].Correct === true) {
-                  return <h2 style={{ color: "green" }}>คำตอบ : {content[counter].Title} ถูก</h2>
-                } else {
-                  return <h2 style={{ color: "red" }}>คำตอบ : {content[counter].Title} ผิด</h2>
+                
+                if (content) {
+                  if (content[counter]?.Correct === true) {
+                    return <h2 style={{ color: "green" }}>คำตอบ : {content[counter]?.Title} ถูก</h2>
+                  } else {
+                    return <h2 style={{ color: "red" }}>คำตอบ : {content[counter]?.Title} ผิด</h2>
+                  }
                 }
-
-
-
-
 
               })}
             </div>
